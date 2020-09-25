5909018b7ef0c65b231cd54ca97c7e3d2f8e8ffc
giveMeBug <389443599@qq.com>
2020-09-25 15:20:37 +0800

    后端部分

--- vote-re/vote.js
+++ vote-re/vote.js
@@ -274,13 +274,13 @@ app.get('/username-conflict-check', async (req, res, next) => {
 })
 
 //获取验证码图片
-app.get('/captcha', function (req, res) {
+/* app.get('/captcha', function (req, res) {
   var captcha = svgCaptcha.create();
   req.session.captcha = captcha.text;
 
   res.type('svg');
   res.status(200).send(captcha.data);
-});
+}); */
 
 app.route('/login')
   .post(async (req, res, next) => {
